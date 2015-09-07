696c51afd68da3659e4d0748afbe47bbdf647609
zhuyawen <zhuyawen1983@126.com>
2015-09-07 20:11:34 +0800

    libs

---
+++ Help/src/com/codezyw/common/UnitHelper.java
@@ -0,0 +1,15 @@
+package com.codezyw.common;
+
+import android.content.Context;
+
+public class UnitHelper {
+	public static float dp2px(Context context, float dp) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return dp * scale + 0.5f;
+    }
+
+    public static float sp2px(Context context, float sp) {
+        final float scale = context.getResources().getDisplayMetrics().scaledDensity;
+        return sp * scale;
+    }
+}
