fb0f424ee9d75b88581826498b753fb7312bbb85
zhuyawen <zhuyawen1983@126.com>
2015-09-16 16:45:33 +0800

    libs

--- Help/src/com/codezyw/common/UnitHelper.java
+++ Help/src/com/codezyw/common/UnitHelper.java
@@ -12,4 +12,44 @@ public class UnitHelper {
         final float scale = context.getResources().getDisplayMetrics().scaledDensity;
         return sp * scale;
     }
+    
+    /**
+     * 输入数字已经乘以万
+     * @param num
+     * @return
+     */
+    public static String formatNumStringWan(long num) {
+        if (num == 0) {
+            return "--";
+        }
+        StringBuffer sb = new StringBuffer();
+        if (num < 0L) {
+            sb.append("-");
+        }
+        num = Math.abs(num);
+        if (num <= 9999L) {
+            String data = String.valueOf(num).trim();
+            sb.append(data);
+            sb.append("万");
+            return sb.toString();
+        } else if (num >= 10000L && num <= 999999L) {
+            String data = String.valueOf(num+50L).trim();
+            int length = data.length();
+            sb.append(data.substring(0, length - 4)).append(".").append(data.substring(length - 4, length - 2));
+            sb.append("亿");
+            return sb.toString();
+        } else if (num >= 1000000L && num <= 9999999L) {
+            String data = String.valueOf(num+500L).trim();
+            int length = data.length();
+            sb.append(data.substring(0, length - 4)).append(".").append(data.substring(length - 4, length - 3));
+            sb.append("亿");
+            return sb.toString();
+        } else {
+            String data = String.valueOf(num+5000L).trim();
+            int length = data.length();
+            sb.append(data.substring(0, length - 4));
+            sb.append("亿");
+            return sb.toString();
+        }
+    }
 }
