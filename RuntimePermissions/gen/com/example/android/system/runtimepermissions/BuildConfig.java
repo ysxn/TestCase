390625737cadf2d1ab0d9c51f341ccac62bd1ced
zhuyawen <zhuyawen1983@126.com>
2015-10-21 14:55:21 +0800

    libs

---
+++ RuntimePermissions/gen/com/example/android/system/runtimepermissions/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.android.system.runtimepermissions;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
